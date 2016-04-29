20496080a807ba9b8ac11faf056d9d40c449972f
한재희 <wogml0123456@naver.com>
2016-04-29 14:43:59 +0900

    키보드로 조작하는 것 구현

--- 오목.c
+++ 오목.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <conio.h>
 #define size 20
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define ENTER 13
+void gotoxy(int x, int y)
+{
+	/*
+	커서의 좌표를 옮기는 함수
+
+	옮길 x좌포와 y좌표를 인자로 받음
+	*/
+	COORD pos = { x, y };				//x와 y좌표를 구조체에 할당
+	HANDLE hnd = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(hnd, pos);	//커서 좌표를 옮김
+}
 
 void TurnChange(char *ptr)
 {
 	/*
 	왼쪽 위에 표시되는 턴을 바꾸는 함수
+
+	오목판 왼쪽 위의 좌표를 인자로 받음
 	*/
 	*ptr = (*ptr == 'O') ? 'X' : 'O';
 }
@@ -13,31 +32,33 @@ void ShowPlate(char(*plate)[20])
 {
 	/*
 	오목판을 보여주는 함수
+
+	오목판 배열을 인자로 받음
 	*/
 	int x, y;
 	HANDLE hnd = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	system("cls");
+	system("cls");						//화면 클리어
 
 	if (plate[0][0] == 'O')				//'O'의 턴이라면
 		SetConsoleTextAttribute(hnd, 0x71); //배경과 글자색 바꿈(옅은 회색, 짙은 파랑)
 	else								//'X'의 턴이라면
 		SetConsoleTextAttribute(hnd, 0x74); //배경과 글자색 바꿈(옅은 회색, 짙은 빨강)
-	printf("%-3c", plate[0][0]);		//턴 출력
+	printf("%2c ", plate[0][0]);		//턴 출력
 
 	SetConsoleTextAttribute(hnd, 0x8F);	//배경과 글자색 바꿈(짙은 회색, 검정)
 	for (x = 1; x < size; x++)
-		printf("%-3d", x);				//1부터 size(20) 전까지 숫자로 출력
+		printf("%2d ", x);				//1부터 size(20) 전까지 숫자로 출력
 	putchar('\n');
 
-	for (y = 1; y < size; y++)			//1부터 size(20) 전까지 
+	for (y = 1; y < size; y++)			
 	{
 		SetConsoleTextAttribute(hnd, 0x8F);//배경과 글자색 바꿈(짙은 회색, 검정)
-		printf("%-3d", plate[y][0]);    //제일 앞에를 숫자로 출력
-		for (x = 1; x < size; x++)		//1부터 size(20) 전까지
+		printf("%2d ", plate[y][0]);    //제일 앞에는 숫자로 출력
+		for (x = 1; x < size; x++)		
 		{
 			SetConsoleTextAttribute(hnd, 0x0F);//배경과 글자색 바꿈(검정, 흰색)
-			printf("%-3c", plate[y][x]);//바둑판 출력
+			printf("%2c ", plate[y][x]);//바둑판 출력
 		}
 		putchar('\n');
 	}
@@ -46,6 +67,8 @@ int Win(char c)
 {
 	/*
 	이겼을 때 출력하는 함수
+
+	이긴 플레이어(O 또는 X)를 인자로 받음
 	*/
 	printf("=======%c 승!======= \n",c);
 	return 1;
@@ -53,7 +76,10 @@ int Win(char c)
 int WinOrNot(char plate[][20], int y, int x)
 {
 	/*
-	이겼는지 졌는지 판단하는 함수
+	끝났는지 아닌지 판단하는 함수
+
+	오목판과 y, x좌표를 인자로 받음
+	끝났을 경우 1, 아니면 0을 반환
 	*/
 	int i, j;
 	int trig = 0;
@@ -115,6 +141,57 @@ int WinOrNot(char plate[][20], int y, int x)
 	}
 	return 0;
 }
+int move(char plate[][20])
+{
+	/*
+	이동을 구현한 함수
+
+	오목판 배열을 인자로 받음
+	게임이 끝났으면 -1, 잘못된 좌표를 선택하면 1, 정상적이면 0 반환
+	*/
+	char clt;
+	int x, y;
+	x = 10 * 3;							//한 칸의 가로가 3이기 때문에, 3을 곱함
+	y = 10;
+	while (1)
+	{
+		clt = _getch();					//키보드로 입력을 받음
+		if (clt == -32 || clt == 0)		//입력받은 키보드가 특수 키코드이면
+			clt = _getch();				//한번 더 읽어야 제대로 된 값 받음
+		
+		if (clt == LEFT)				//왼쪽키이면
+			x -= 3;						//왼쪽으로 세 칸(한 블럭)이동
+		else if (clt == RIGHT)			//오른쪽 키이면
+			x += 3;						//오른쪽으로 세 칸(한 블럭)이동
+		else if (clt == UP)				//위쪽 키이면
+			y -= 1;						//위로 한 칸 이동
+		else if (clt == DOWN)			//아래쪽 키이면
+			y += 1;						//아래쪽으로 한 칸 이동
+		else if (clt == ENTER)			//엔터키이면
+		{
+			if (plate[y][x/3] != '.')			//좌표가 비어있지 않으면
+				return 1;						//1을 반환해 오류 메세지 출력 트리거 활성화
+			else								//비어있으면
+				plate[y][x / 3] = plate[0][0];  //돌을 놓음
+
+			if (WinOrNot(plate, y, x / 3))		//만약 게임이 끝났으면
+			{
+				ShowPlate(plate);				//바둑판을 보여주고
+				Win(plate[0][0]);				//Win() 실행
+				return -1;						//-1 반환
+			}
+			else								//끝나지 않았으면
+			{
+				return 0;						//0 반환
+			}
+		}
+
+		gotoxy(0, 22);							//(0, 22)지점으로 이동해서
+		printf("좌표 : %-2d %-2d", x / 3, y);	//출력
+
+		gotoxy(x+1, y);							//좌표 이동
+	}
+}
 int main(void)
 {
 	char plate[20][20];					//오목판 배열
@@ -139,20 +216,12 @@ int main(void)
 			printf("잘못된 좌표 입력, 턴 넘어감 \n");//오류 메세지 출력
 			trig = 0;					//트리거 비활성화
 		}
-		printf("좌표? ");
-		scanf("%d %d", &x, &y);			//좌표 입력
+		gotoxy(10 * 3 + 1, 10);			//(10, 10) 지점으로 커서 이동
+		trig = move(plate);				//트리거에 move()가 반환한 값 저장
+		
+		if (trig == -1)					//-1이 반환되었으면
+			return 0;					//종료
 
-		if (plate[y][x] != '.')			//좌표가 비어있지 않으면
-			trig = 1;					//오류 메세지 출력 트리거 활성화
-		else							//비어있으면
-			plate[y][x] = plate[0][0];  //돌을 놓음
-
-		if( WinOrNot(plate, y, x) )		//만약 게임이 끝났으면
-		{
-			ShowPlate(plate);			//바둑판을 보여주고
-			Win(plate[0][0]);			//Win() 실행
-			return 0;					//끝냄
-		}
 		TurnChange(&plate[0][0]);		//턴 바꿈
 	}
 }
